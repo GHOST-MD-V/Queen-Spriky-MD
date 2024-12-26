0ff1b8716bbabcd39bc6bd003b0f4bd3054b37c0
GHOST-MD-V <heshankavindu838@gmail.com>
2024-12-26 08:55:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'PRABATH-MD~IvkU3TpD#w86wDgivD1YePT6QGzk-TUn4wCQ7RvtS4h-ESvomdQU', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94715898396',    // Enter Your Owner Number
 };
